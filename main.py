a70321819a8532fab4b027fb00526abca99135b7
ashleylqx <ashleylqx@gmail.com>
2020-02-06 18:20:56 +0800

    test A4 aug7 fdim512 layer34 cw sa_art_ftf_2_nob_mres_sp (att, linear_out with res) multi&res, no grad from sp, lr=1e-5 <init with best, alt_3 during finetuning>

--- main.py
+++ main.py
@@ -4303,7 +4303,7 @@ def main_Wildcat_WK_hd_compf_map(args):
     # phase = 'test'
     # phase = 'test_cw'
     # phase = 'test_cw_sa'
-    # phase = 'test_cw_sa_sp_multiscale'
+    phase = 'test_cw_sa_sp_multiscale'
     # phase = 'test_cw_sa_sp'
     # phase = 'test_cw_ils_tgt'
 
@@ -4311,7 +4311,7 @@ def main_Wildcat_WK_hd_compf_map(args):
     # phase = 'train_cw_aug_gbvs'
     # phase = 'train_cw_aug_sa_new'
     # phase = 'train_cw_aug_sa_art'
-    phase = 'train_cw_aug_alt_alpha_sa_sp'
+    # phase = 'train_cw_aug_alt_alpha_sa_sp'
     # phase = 'train_alt_alpha_sa_new'
     # phase = 'train_cw_aug_sa_sp' # sa_new_sp, sa_art_sp
     # phase = 'train_cw_aug_sa'
@@ -9214,7 +9214,7 @@ def main_Wildcat_WK_hd_compf_map(args):
         # folder_name = 'Preds/PASCAL-S'
         folder_name = 'Preds/MIT1003'
         # best_model_file = 'no_training'
-        E_NUM = [2,3,4]
+        E_NUM = [0]
         # E_NUM.extend(list(range(5,16)))
         prior = 'nips08'
 
@@ -9236,7 +9236,7 @@ def main_Wildcat_WK_hd_compf_map(args):
                 #
 
             else:
-                best_model_file = 'resnet50_wildcat_wk_hd_cbA{}_compf_cls_att_gd_nf4_norm{}_hb_{}_aug7_{}_rf{}_hth{}_ms4_fdim{}_34_cw_sa_art_ftf_2_nob_mres_sp_kmax{}_kmin{}_a{}_M{}_f{}_dl{}_one3_224_epoch{:02d}'.format(
+                best_model_file = 'resnet50_wildcat_wk_hd_cbA{}_alt_3_compf_cls_att_gd_nf4_norm{}_hb_{}_aug7_{}_rf{}_hth{}_ms4_fdim{}_34_cw_sa_art_ftf_2_nob_mres_sp_kmax{}_kmin{}_a{}_M{}_f{}_dl{}_one3_224_epoch{:02d}'.format(
                     n_gaussian, normf, MAX_BNUM, prior, rf_weight, hth_weight, FEATURE_DIM, kmax, kmin, alpha, num_maps,
                     fix_feature, dilate, e_num)  # _gcn_all
                 # best_model_file = 'resnet50_wildcat_wk_hd_cbA{}_compf_cls_att_gd_nf4_norm{}_hb_{}_aug7_{}_rf{}_hth{}_ms4_fdim{}_34_cw_sa_new_kmax{}_kmin{}_a{}_M{}_f{}_dl{}_one3_224_epoch{:02d}'.format(
