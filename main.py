d2fa5f31e0f011044e164fc768380cf64daec8cc
ashleylqx <ashleylqx@gmail.com>
2020-02-14 00:14:24 +0800

    test A4 aug7 fdim512 layer34 cw sa_art_ftf_2_mres_sp (att, linear_out with res) multi&res, grad from sp, lr=1e-5 <init with fixf> SALICON test

--- main.py
+++ main.py
@@ -2436,7 +2436,8 @@ def test_Wildcat_WK_hd_compf_multiscale_cw_sa_sp(model, folder_name, best_model_
         # ori_img = scipy.misc.imread(os.path.join(PATH_MIT1003, 'ALLSTIMULI', img_name[0] + '.jpeg'))  # height, width, channel
         # ori_img = scipy.misc.imread(os.path.join(PATH_PASCAL, 'images', img_name[0]+'.jpg')) # height, width, channel
         # ori_img = scipy.misc.imread(os.path.join(PATH_MIT300, 'images', img_name[0]+'.jpg')) # height, width, channel
-        ori_img = scipy.misc.imread(os.path.join(PATH_SALICON, 'images', 'val', img_name[0]+'.jpg')) # height, width, channel
+        # ori_img = scipy.misc.imread(os.path.join(PATH_SALICON, 'images', 'val', img_name[0]+'.jpg')) # height, width, channel
+        ori_img = scipy.misc.imread(os.path.join(PATH_SALICON, 'images', 'test', img_name[0]+'.jpg')) # height, width, channel
 
         ori_size = ori_inputs.size(-1)
         pred_maps_all = torch.zeros(ori_inputs.size(0), 1, output_h, output_w).to(ori_inputs.device)
@@ -9933,14 +9934,16 @@ def main_Wildcat_WK_hd_compf_map(args):
         # folder_name = 'Preds/PASCAL-S'
         # folder_name = 'Preds/MIT1003'
         # folder_name = 'Preds/MIT300'
-        folder_name = 'Preds/SALICON'
+        # folder_name = 'Preds/SALICON' #validation set
+        folder_name = 'Preds/SALICON_test'
         # best_model_file = 'no_training'
         E_NUM = [3]
         # E_NUM.extend(list(range(5,16)))
         prior = 'nips08'
         args.batch_size = 1
 
-        ds_test = SALICON_full(return_path=True, img_h=input_h, img_w=input_w, mode='val')  # N=4,
+        # ds_test = SALICON_full(return_path=True, img_h=input_h, img_w=input_w, mode='val')  # N=4,
+        ds_test = SALICON_full(return_path=True, img_h=input_h, img_w=input_w, mode='test')  # N=4,
         test_dataloader = DataLoader(ds_test, batch_size=args.batch_size, collate_fn=collate_fn_salicon_rn,
                                      shuffle=False, num_workers=2)
 
