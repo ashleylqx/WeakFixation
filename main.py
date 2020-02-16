147a48bcacc88fd881d101ada0b516147bc86a7d
ashleylqx <ashleylqx@gmail.com>
2020-02-16 15:51:23 +0800

    test A4 aug7 fdim512 layer34 cw rf=0.0 no prior loss, sa_art_ftf_2_sp (att, linear_out with res) lr=1e-5

--- main.py
+++ main.py
@@ -52,9 +52,9 @@ from utils import *
 from tensorboardX import SummaryWriter
 
 cps_weight = 1.0
-hth_weight = 0.0 #0.1 #1.0 #
+hth_weight = 0.1 #0.1 #1.0 #
 hdsup_weight = 0.1  # 0.1, 0.1
-rf_weight = 0.1 #0.1 #1.0 #
+rf_weight = 0.0 #0.1 #1.0 #
 
 
 # run = 'hd_gs_A{}_gd_nf4_normT_eb_{}_aug7_a_A4_fdim{}_34_bms_thm'.format(n_gaussian, MAX_BNUM, FEATURE_DIM, BMS_R) # 1.0
@@ -10769,7 +10769,7 @@ def main_Wildcat_WK_hd_compf_map(args):
         # folder_name = 'Preds/SALICON' #validation set
         # folder_name = 'Preds/SALICON_test'
         # best_model_file = 'no_training'
-        E_NUM = [0,1,2,6]
+        E_NUM = [0,1,2,3,8]
         # E_NUM.extend(list(range(5,16)))
         prior = 'nips08'
         args.batch_size = 1
