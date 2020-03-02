3045a98ea57a5459f2d1ccb1a0b2b75de2d80b7b
ashleylqx <ashleylqx@gmail.com>
2020-03-02 12:31:49 +0800

    test A4 aug7 fdim512 layer34 cw all_4 (more data, hth)

--- main.py
+++ main.py
@@ -11520,8 +11520,8 @@ def main_Wildcat_WK_hd_compf_map(args):
             # best_model_file = 'resnet50_wildcat_wk_hd_cbA{}_compf_cls_att_gd_nf4_norm{}_hb_{}_proa_{}_aug7_nips08_rf{}_hth{}_ms4_kmax{}_kmin{}_a{}_M{}_f{}_dl{}_one3_224_epoch{:02d}'.format(
             #     n_gaussian, normf, MAX_BNUM, PRO_RATIO, rf_weight, hth_weight, kmax, kmin, alpha, num_maps, fix_feature, dilate, e_num)  # _gcn_all
 
-            best_model_file = 'resnet50_wildcat_wk_hd_cbA{}_all_4_compf_cls_att_gd_nf4_norm{}_hb_{}_aug7_{}_rf{}_hth{}_ms4_kmax{}_kmin{}_a{}_M{}_f{}_dl{}_one3_224_epoch{:02d}'.format(
-                n_gaussian, normf, MAX_BNUM, prior, rf_weight, hth_weight, kmax, kmin, alpha, num_maps, fix_feature,dilate, e_num)  # _gcn_all
+            best_model_file = 'resnet50_wildcat_wk_hd_cbA{}_all_4_compf_cls_att_gd_nf4_norm{}_hb_{}_aug7_nips08_rf{}_hth{}_ms4_kmax{}_kmin{}_a{}_M{}_f{}_dl{}_one3_224_epoch{:02d}'.format(
+                n_gaussian, normf, MAX_BNUM, rf_weight, hth_weight, kmax, kmin, alpha, num_maps, fix_feature,dilate, e_num)  # _gcn_all
 
             # best_model_file = 'resnet50_wildcat_wk_hd_cbG{}_alt_4_compf_cls_att_gd_nf4_norm{}_hb_{}_aug7_nips08_rf{}_hth{}_ms4_kmax{}_kmin{}_a{}_M{}_f{}_dl{}_one3_224_epoch{:02d}'.format(
             #                                 n_gaussian, normf, MAX_BNUM, rf_weight, hth_weight,kmax,kmin,alpha,num_maps,fix_feature, dilate, e_num) #_gcn_all
